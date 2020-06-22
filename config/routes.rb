f62b5f8234de58c512c83ce22760f319e3538beb
toi1989 <migiwanagisa@gmail.com>
2020-06-23 00:17:13 +0900

    group

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+=======
+  devise_for :users
+  root "messages#index"
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
+
+>>>>>>> Stashed changes
 end
