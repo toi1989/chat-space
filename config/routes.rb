e2c137eeec840a5c55a4501cfaccf585cdb93a10
toi1989 <migiwanagisa@gmail.com>
2020-06-23 21:22:35 +0900

    group

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :users, only: [:edit, :update]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+  root "messages#index"
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
+
 end
