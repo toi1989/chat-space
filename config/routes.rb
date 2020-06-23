ee3d47167802421d39607071c6354f502b4bed04
toi1989 <migiwanagisa@gmail.com>
2020-06-23 20:45:35 +0900

    Revert "group"
    This reverts commit f62b5f8234de58c512c83ce22760f319e3538beb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
-  devise_for :users
-  root "messages#index"
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]
-
->>>>>>> Stashed changes
 end
