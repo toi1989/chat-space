bdd907c8ea38bd992567c751d4961be71a7a7e7d
toi1989 <migiwanagisa@gmail.com>
2020-06-23 21:46:05 +0900

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root "messages#index"
-  resources :users, only: [:edit, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 
   devise_for :users
   root "messages#index"
