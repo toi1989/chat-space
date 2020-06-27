f59fd9bcc3698ff3b9311dcf13ecfaad4cbaa1b5
toi1989 <migiwanagisa@gmail.com>
2020-06-27 18:08:30 +0900

    フロント

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
     devise_for :users
     root 'groups#index'
     resources :users, only: [:edit, :update]
-    resources :groups, only: [:new, :create, :edit, :update] do
+    resources :groups, only: [:index, :new, :create, :edit, :update] do
       resources :messages, only: [:index, :create]
     end
   end
