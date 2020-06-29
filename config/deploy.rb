5b4e7199fd087cef9fc77bc172dc88a8aac769a8
toi1989 <migiwanagisa@gmail.com>
2020-06-30 01:29:53 +0900

    key

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/key_chat.pem'] 
+                  keys: ['~/.ssh/chat.pem'] 
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
