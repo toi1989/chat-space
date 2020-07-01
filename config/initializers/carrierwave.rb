83feea6bb490ca09bdebb5f20d52d606ee628248
toi1989 <migiwanagisa@gmail.com>
2020-07-01 20:34:25 +0900

    パケット名

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'ここにバケット名を入れます'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.fog_directory  = 'chat-space1989toi'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space1989toi'
 end
